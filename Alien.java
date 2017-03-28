56a1ec082cc9beee3421d5f5a2109a5a0f7b22c3
Matias Mulassi <mulassimatias@gmail.com>
2017-03-28 15:18:04 -0300

    Initial Comitte

---
+++ Alien.java
@@ -0,0 +1,15 @@
+package cacho;
+
+public class Alien {
+
+	 String nombre;
+	static int signumeroDeAlien;
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("Nenoneno....");
+	}
+
+}
