da0cbb1e58ee0344b9b14803427c6b814ca318be
Matias <Mulassimatias@gmail.com>
2017-03-31 17:47:53 -0300

    Add ejercicio 1 and 2

---
+++ src/Ejercicio1.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Ejercicio1 {
+
+	public static void main(String[] arg){
+		
+		int[] nros= new int[10];
+		
+Scanner s = new Scanner(System.in);
+		
+		for(int j=0;j<10;j++){
+		System.out.println("Ingrese dato");
+		String dato = s.nextLine();
+		nros[j] = Integer.parseInt(dato);
+		}
+		
+		for (int k=9;k>=0;k--){
+			
+			System.out.println(""+nros[k]);
+			
+			}
+		s.close();
+
+
+}
+
+
+}
---
+++ src/Ejercicio2.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+public class Ejercicio2 {
+
+	
+
+		public static void main(String[] arg){
+			
+			int nro=0,c=0;
+			int[] nros= new int[20];
+			int[] nrosor= new int[20];
+			
+	Scanner s = new Scanner(System.in);
+			
+			System.out.println("Ingrese 1er numero");
+			String dato = s.nextLine();
+			nro= Integer.parseInt(dato);
+			System.out.println("");
+			
+			
+			for (int k=0;k<20;k++){
+				
+				System.out.println("Ingrese numero "+k);
+				String num = s.nextLine();
+				nros[k]= Integer.parseInt(num);
+				System.out.println("");	
+			}
+			s.close();
+			
+			for (int l=0;l<20;l++){
+				if (nros[l]>nro) 
+					{ nrosor[c]=nros[l];
+						c++;
+									};
+			
+							}
+
+
+				
+			for (int g=0;g<=c;g++){
+				
+						System.out.println(""+nrosor[g]);
+				
+				
+			}						}	
+
+
+	
+	
+													}
